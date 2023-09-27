6bc94bfa81387be477a3c0283bcf8ed6dae6ea82
engkos kostaman <engkos.kostaman@bni.co.id>
2023-09-27 17:34:22 +0700

    spring restful api

---
+++ src/test/java/engkoskostaman/belajarspringrestfullapi/BelajarSpringRestfullApiApplicationTests.java
@@ -0,0 +1,13 @@
+package engkoskostaman.belajarspringrestfullapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringRestfullApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
